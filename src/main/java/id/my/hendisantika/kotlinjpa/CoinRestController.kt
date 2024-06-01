12a8d7bcaa536849187d54e80b2722eb8dfa2d4b
hendisantika <hendisantika@yahoo.co.id>
2024-06-01 17:00:07 +0700

    Add CoinRestController🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/kotlinjpa/CoinRestController.kt
@@ -0,0 +1,21 @@
+package id.my.hendisantika.kotlinjpa
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : kotlin-jpa
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 01/06/24
+ * Time: 16.59
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+class CoinRestController {
+
+    @Autowired
+    lateinit var coinRepository: CoinRepository
+}
